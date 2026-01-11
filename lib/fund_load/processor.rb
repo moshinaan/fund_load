1b355712a1ca4940f687ce07651657f8ffa29503
Anastasiia Moshina <moshinaan@gmail.com>
2026-01-11 15:22:35 +0400

    private methods for limits

--- lib/fund_load/processor.rb
+++ lib/fund_load/processor.rb
@@ -20,19 +20,13 @@ module FundLoad
       day_key = time.to_date.iso8601
       week_key = [time.to_date.cwyear, time.to_date.cweek]
 
-      attempts_today = @daily_attempts[customer_id][day_key]
-      over_attempt_limit = attempts_today >= DAILY_ATTEMPT_LIMIT
-
       amount_cents = parse_amount_cents(data.fetch('load_amount'))
 
-      # Assumption: daily/weekly amount limits track only accepted loads.
-      daily_total = @daily_amounts[customer_id][day_key]
-      weekly_total = @weekly_amounts[customer_id][week_key]
-
-      over_daily_amount = (daily_total + amount_cents) > DAILY_AMOUNT_LIMIT_CENTS
-      over_weekly_amount = (weekly_total + amount_cents) > WEEKLY_AMOUNT_LIMIT_CENTS
-
-      accepted = !(over_attempt_limit || over_daily_amount || over_weekly_amount)
+      accepted = !(
+        limit_attempt(customer_id, day_key) ||
+        limit_over_daily_amount(customer_id, day_key, amount_cents) ||
+        limit_over_weekly_amount(customer_id, week_key, amount_cents)
+      )
 
       @daily_attempts[customer_id][day_key] += 1
       if accepted
@@ -45,12 +39,27 @@ module FundLoad
 
     private
 
-    # Assumption: week boundaries follow ISO-8601 weeks (Monday start).
     def parse_amount_cents(amount)
       normalized = amount.delete('$')
       dollars, cents = normalized.split('.', 2)
       cents = cents.to_s.ljust(2, '0')[0, 2]
+
       (dollars.to_i * 100) + cents.to_i
+    end
+
+    def limit_attempt(customer_id, day_key)
+      attempts_today = @daily_attempts[customer_id][day_key]
+      attempts_today >= DAILY_ATTEMPT_LIMIT
+    end
+
+    def limit_over_daily_amount(customer_id, day_key, amount_cents)
+      daily_total = @daily_amounts[customer_id][day_key]
+      (daily_total + amount_cents) > DAILY_AMOUNT_LIMIT_CENTS
+    end
+
+    def limit_over_weekly_amount(customer_id, week_key, amount_cents)
+      weekly_total = @weekly_amounts[customer_id][week_key]
+      (weekly_total + amount_cents) > WEEKLY_AMOUNT_LIMIT_CENTS
     end
   end
 end
